02ee3efc8af80fba7a8d914b37578da841a6daaa
Johan Ehrendahl <jonnyboy0719@gmail.com>
2023-12-17 00:33:14 +0100

    Increased machete distance

--- dlls/machete.cpp
+++ dlls/machete.cpp
@@ -107,7 +107,7 @@ void CMachete::SecondaryAttack()
 
 	UTIL_MakeVectors(m_pPlayer->pev->v_angle);
 	Vector vecSrc = m_pPlayer->GetGunPosition();
-	Vector vecEnd = vecSrc + gpGlobals->v_forward * 32;
+	Vector vecEnd = vecSrc + gpGlobals->v_forward * 55;
 
 	UTIL_TraceLine(vecSrc, vecEnd, dont_ignore_monsters, ENT(m_pPlayer->pev), &tr);
 
